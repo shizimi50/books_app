b1983f252f42e083b27ee2eee0f3206228aaa6cb
shizimi50 <mjrwrbbc@gmail.com>
2022-07-30 16:52:44 +0900

    rubocopファイル作成、実行

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,14 +12,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_30_064445) do
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.text "memo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "author"
-    t.string "picture"
+ActiveRecord::Schema[7.0].define(version: 20_220_730_064_445) do
+  create_table 'books', force: :cascade do |t|
+    t.string 'title'
+    t.text 'memo'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'author'
+    t.string 'picture'
   end
-
 end
